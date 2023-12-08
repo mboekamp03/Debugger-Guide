ac1b6c4fe7019d702fba2618a8a142fbb99323b7
Matt Boekamp <98491428+mboekamp03@users.noreply.github.com>
2023-12-08 08:47:57 -0500

    Create add.py

---
+++ example/add.py
@@ -0,0 +1,12 @@
+def add_numbers(a, b):
+    sum = a + c
+    return sum
+
+def print_message(number):
+    message = f"The sum is {number}"
+    print(message)
+
+number1 = 5
+number2 = 3
+result = add_numbers(number1, number2)
+print_message(result)
