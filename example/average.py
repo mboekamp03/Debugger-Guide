8f3f15741e289864249c55adc5481a23f18e2b4a
Matt Boekamp <matthew_boekamp@uri.edu>
2023-12-10 11:52:45 -0500

    add additional practice program, tour for the program, and reflection

---
+++ example/average.py
@@ -0,0 +1,30 @@
+def calculate_sum(numbers):
+    """ Function to calculate the sum of a list of numbers. """
+    total = 0
+    for i in range(len(numbers)):
+        total += numbers[i + 1]
+    return total
+
+def find_average(numbers):
+    """ Function to find the average of a list of numbers. """
+    total = calculate_sum(numbers)
+    average = total / len(numbers)
+    return average
+
+def main():
+    # Define a list of numbers
+    numbers = [10, 20, 30, 40, 50]
+
+    # Calculate the sum of the numbers
+    sum_of_numbers = calculate_sum(numbers)
+    print(f"The sum of the numbers is: {sum_of_numbers}")
+
+    # Calculate the average of the numbers
+    average_of_numbers = find_average(numbers)
+    print(f"The average of the numbers is: {average_of_numbers}")
+
+    # Accessing an index that does not exist (intentional error)
+    print(f"Accessing a non-existing index: {numbers[10]}")
+
+# Run the main function
+main()
